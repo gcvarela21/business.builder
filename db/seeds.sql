33193a6398901ccdf9784fff01079ef1de6cca16
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-03-03 14:09:02 -0800

    add seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+---- Admin Login ----
+INSERT INTO Users (username, password); 
+VALUES ("admin", "password");
+
+---- Items (All) ----
+INSERT INTO items (name, itemCategory, description, price) VALUES ("Blue Cake", "Cakes", "Really delicous cake that I can't wait to eat.", 50.00);
+
+INSERT INTO items (name, itemCategory, description, price) VALUES ("Chocolate Chip Cookie", "Cookies", "Only the best kind of cookie known to man.", 5.00);
+
+
+
