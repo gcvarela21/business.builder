49e516bdef64110b74132bcc1c31472be6b8a814
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-03-03 12:12:59 -0800

    add placeholder header

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 import Home from "./pages/Home.js"
 
 function App() {
   return (
     <>
       <Router>
-
+        <Header />
         <NavBar />
         {/* <Switch> */}
         <Route exact path="/" component={Home} />
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+function Header() {
+    return (
+        <>
+            <img src="https://via.placeholder.com/1500x200/000000/FFFFFF/?text=Danielle" />
+        </>
+    )
+}
+
+export default Header;
