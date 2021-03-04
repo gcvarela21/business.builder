292c584273dd05938eb965c9ce53b0f50f421d9a
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-03-03 17:14:14 -0800

    add to gallery

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
-import Home from "./pages/Home.js"
-import Shop from "./pages/Shop.js"
+import Home from "./pages/Home.js";
+import Shop from "./pages/Shop.js";
+import Gallery from "./pages/Gallery.js";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         {/* <Switch> */}
         <Route exact path="/" component={Home} />
         <Route exact path="/shop" component={Shop} />
+        <Route exact path="/gallery" component={Gallery} />
         {/* </Switch> */}
         {/* <Shop /> */}
       </Router>
