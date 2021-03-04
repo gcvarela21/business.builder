a8d218dac9d1352876f6a01fdb4d35663de69968
Peter Ting <pting1995@gmail.com>
2021-03-03 17:06:41 -0800

    updated controller, db seeds, model, package.json and server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,9 +3,9 @@ INSERT INTO Users (username, password);
 VALUES ("admin", "password");
 
 ---- Items (All) ----
-INSERT INTO items (name, itemCategory, description, price) VALUES ("Blue Cake", "Cakes", "Really delicous cake that I can't wait to eat.", 50.00);
+INSERT INTO items (itemName, itemCategory, itemDesc, itemPrice) VALUES ("Blue Cake", "Cakes", "Really delicous cake that I can't wait to eat.", 50.00);
 
-INSERT INTO items (name, itemCategory, description, price) VALUES ("Chocolate Chip Cookie", "Cookies", "Only the best kind of cookie known to man.", 5.00);
+INSERT INTO items (itemName, itemCategory, itemDesc, itemPrice) VALUES ("Chocolate Chip Cookie", "Cookies", "Only the best kind of cookie known to man.", 5.00);
 
 
 
