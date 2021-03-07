559d974f671920ff86a5f4eb032d69e07fe01fd7
Peter Ting <pting1995@gmail.com>
2021-03-07 15:38:57 -0800

    Added api call to get more info about local storage itemIDs

--- client/src/pages/CartCheckout.js
+++ client/src/pages/CartCheckout.js
@@ -14,21 +14,36 @@ function CartCheckout() {
     function getMyCartItems() {
         var holderArr = []
         for (var i = 0; i < localStorage.length; i++) {
+            // gets itemID and quantity from local storage
             var key = window.localStorage.key(i);
             var value = JSON.parse(window.localStorage.getItem(localStorage.key(i)));
+
             // console.log(key)
             // console.log(value)
-            holderArr.push({ [key]: value });
-            getItemInfo(key)
+            // look for itemInfo from the key in local storage
+            holderArr.push({ "itemID": key, "itemQuantity": value })
+        }
+        for (var j = 0; j < holderArr.length; j++) {
+            // console.log(holderArr)
+            API.getItem(holderArr[j].itemID)
+                .then(res => holderArr[j] = (res.data))
         }
         // console.log(holderArr)
+        // i want to add item quantity (from local) + desc, title, id, img to holderArr 
+        // E.G.
+        // [    
+        // {itemQuantity: 5
+        // itemName: cOOKIE
+        // itemDesc: ITS GOOD}
+        // ]
+        // console.log(holderArr)
         return setCartItems(holderArr)
-    }
 
-    function getItemInfo(id) {
-        console.log(id)
-        API.getItem(id)
     }
+    // THIS KINDA WORKED IDK
+    // API.getItem(key)
+    //     .then(res => holderArr.push({ [key]: res.data.key, "itemQuantity": value }))
+    //     .catch(err => console.log(err));
     console.log(cartItems)
     return (
         <>
