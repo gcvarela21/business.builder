0e57fe5dfcdc0cd59bc435db18878d52ad802c12
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-03-07 22:24:39 -0800

    add image prop

--- client/src/components/ItemCard/index.js
+++ client/src/components/ItemCard/index.js
@@ -7,7 +7,7 @@ function ItemCard(props) {
     return (
         <>
             <div className="card" style={{ width: 300 }}>
-                <img src="https://via.placeholder.com/200" className="card-img-top" alt="..." />
+                <img src={props.itemImg}className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{props.itemName}</h5>
                     <p>{props.itemDesc}</p>
