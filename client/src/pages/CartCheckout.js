cf1b6b806bcfd868ce365d46fdb8ef3fdbe93b8a
Peter Ting <pting1995@gmail.com>
2021-03-08 11:43:24 -0800

    moved client side stripe to utils folder

--- client/src/pages/CartCheckout.js
+++ client/src/pages/CartCheckout.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import API from "../utils/API";
 import Checkout from "../components/Checkout"
 import { loadStripe } from "@stripe/stripe-js";
-import axios from "axios"
+import StripeUtil from "../utils/StripeUtil";
+
 
 function CartCheckout(props) {
     const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
@@ -27,33 +28,7 @@ function CartCheckout(props) {
 
     // stripe stuff to whisk the patron away
     const handleClick = async () => {
-        console.log("stripe was pressed")
-        const stripe = await stripePromise;
-        // execute total here
-        // transaction name - maybe stripe id or 
-        axios.post("/create-checkout-session", { cartItems: cartItems })
-            .then(async function (data) {
-                const result = await stripe.redirectToCheckout({
-                    sessionId: data.data.id,
-                });
-            })
-            .catch(function (err) {
-                console.log(err)
-            })
-        // console.log(response)
-        // const response = await fetch("/create-checkout-session", {
-        //     method: "POST",
-        //     body: JSON.stringify({ cartItems: cartItems })
-        // });
-        // console.log(cartItems)
-        // const session = await response.json();
-        // When the customer clicks on the button, redirect them to Checkout.
-
-        // if (result.error) {
-        //     // If `redirectToCheckout` fails due to a browser or network
-        //     // error, display the localized error message to your customer
-        //     // using `result.error.message`.
-        // }
+        StripeUtil.stripeCheckout(cartItems)
     };
 
     return (
---
+++ client/src/utils/StripeUtil.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripeUtil = {
+    stripeCheckout: async function (cartItems) {
+        const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+        const stripe = await stripePromise;
+        return axios.post("/create-checkout-session", { cartItems })
+            .then(async function (data) {
+                const result = await stripe.redirectToCheckout({
+                    sessionId: data.data.id,
+                });
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+        // console.log(response)
+        // const response = await fetch("/create-checkout-session", {
+        //     method: "POST",
+        //     body: JSON.stringify({ cartItems: cartItems })
+        // });
+        // console.log(cartItems)
+        // const session = await response.json();
+        // When the customer clicks on the button, redirect them to Checkout.
+
+        // if (result.error) {
+        //     // If `redirectToCheckout` fails due to a browser or network
+        //     // error, display the localized error message to your customer
+        //     // using `result.error.message`.
+        // }
+    }
+}
+
+export default stripeUtil;
