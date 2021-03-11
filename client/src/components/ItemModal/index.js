1e9349c7fb5dc97f35aa9fc0281d0901a724cd30
Peter Ting <pting1995@gmail.com>
2021-03-10 22:41:25 -0800

    removed warning

--- client/src/components/ItemModal/index.js
+++ client/src/components/ItemModal/index.js
@@ -19,16 +19,12 @@ function ItemModal(props) {
         event.preventDefault()
         addToLocal(event)
         // notieAlert()
-        // eslint-disable-next-line no-restricted-globals
-        // location.reload()
     }
 
     function addToLocal() {
 
         var itemQuantity = document.getElementById([props.id]).value
         var itemPriceCent = (props.itemPrice) * 100
-        var itemPickUpDay = document.getElementById("pickup-day").value
-        console.log(itemPickUpDay)
         var itemInfo = {
             id: props.id,
             itemQuantity: parseInt(itemQuantity),
@@ -36,7 +32,6 @@ function ItemModal(props) {
             itemDesc: props.itemDesc,
             itemImg: props.itemImg,
             itemPriceCent: itemPriceCent,
-            itemPickUpDay: itemPickUpDay
         }
 
         localStorage.setItem(props.id, JSON.stringify(itemInfo));
@@ -60,15 +55,6 @@ function ItemModal(props) {
                             <label htmlFor="exampleInputEmail1" className="form-label"><strong>Enter Quantity: </strong></label>
                             <input type="number" className="form-control" id={props.id}></input>
                             <p className="pickup-text"><small><strong>Pickup info: </strong>Orders receieved by Wednesday @ 11:59pm will be available for pickup Fri, Sat, & Sun between the hours of 12-4pm. Any orders received after this time will be availble the following week at the same times. Please use the contact form to inquire about special pickup options or to place orders for pickup more than one week in advance.</small></p>
-
-
-                            <select id="pickup-day" name="pickup-day">
-                                <option value="friday">Friday @ SF Bakery Location: 12-4pm</option>
-                                <option value="saturday">Saturday @ Speakeasy Brewery SF: 12-4pm</option>
-                                <option value="sunday">Sunday @ Speakeasy Brewery SF: 12-4pm</option>
-                            </select>
-
-
                             <button type="submit" className="btn btn-dark" >Add To Cart</button>
                         </form>
                     </div>
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,14 +3,14 @@ import React from "react";
 function About() {
     return (
         <div className="container about">
-            <img className="about-img" style={{float: "left", marginRight: 20}} src="https://i.etsystatic.com/iusa/8e3b2c/62416622/iusa_400x400.62416622_1uxz.jpg?version=0" />
-        <p>
-        Danielle is a Bay Area native and has had a passion for baking since her early years. She recalls baking around the holidays and giving treats out to family and friends while fantasizing of someday opening her own bakery. However, this remained merely a fantasy as her strict household dictated she follow a more traditional path leading her to study Economics at San Diego State.</p>
-        
-       <p> Upon graduating she moved back to the Bay Area where she finally decided to pursue her lifelong dream. She enrolled at Le Cordon Bleu and for the first time in her life was excited about school. Here she combined her newly found technical skills and real-world experience, externing at not one, but two, of the Bay Area’s top bakeries. </p>
-        
-        <p>At Spoonful of Sugar she learned modern wedding cake techniques while she credits her work under Jasmine Rae for helping her find her artistic and creative voice. </p>
-        
+            <img className="about-img" style={{ float: "left", marginRight: 20 }} src="https://i.etsystatic.com/iusa/8e3b2c/62416622/iusa_400x400.62416622_1uxz.jpg?version=0" alt="danielle" />
+            <p>
+                Danielle is a Bay Area native and has had a passion for baking since her early years. She recalls baking around the holidays and giving treats out to family and friends while fantasizing of someday opening her own bakery. However, this remained merely a fantasy as her strict household dictated she follow a more traditional path leading her to study Economics at San Diego State.</p>
+
+            <p> Upon graduating she moved back to the Bay Area where she finally decided to pursue her lifelong dream. She enrolled at Le Cordon Bleu and for the first time in her life was excited about school. Here she combined her newly found technical skills and real-world experience, externing at not one, but two, of the Bay Area’s top bakeries. </p>
+
+            <p>At Spoonful of Sugar she learned modern wedding cake techniques while she credits her work under Jasmine Rae for helping her find her artistic and creative voice. </p>
+
         </div>
     )
 
